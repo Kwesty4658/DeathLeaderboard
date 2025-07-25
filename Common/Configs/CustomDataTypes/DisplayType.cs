aa94f0a1be7860563da85a4221ec43e4fefb3966
Kwesty4658 <conor.kwesty@outlook.com>
2025-07-25 19:27:55 +0100

    moved DisplayType to match ExampleMod

---
+++ Common/Configs/CustomDataTypes/DisplayType.cs
@@ -0,0 +1,14 @@
+using Terraria.ModLoader.Config;
+
+namespace DeathLeaderboard.Common.Configs.CustomDataTypes;
+
+public enum DisplayType
+{
+    [LabelKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.Deaths.Label")]
+    [TooltipKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.Deaths.Tooltip")]
+    Deaths,
+
+    [LabelKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.DeathsCauses.Label")]
+    [TooltipKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.DeathsCauses.Tooltip")]
+    DeathsCauses
+}
--- Common/Configs/LeaderboardFormattingConfig.cs
+++ Common/Configs/LeaderboardFormattingConfig.cs
@@ -1,4 +1,5 @@
 using Terraria.ModLoader.Config;
+using DeathLeaderboard.Common.Configs.CustomDataTypes;
 
 namespace DeathLeaderboard.Common.Configs;
 
@@ -10,15 +11,4 @@ public class LeaderboardFormattingConfig : ModConfig
     [LabelKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.Label")]
     [TooltipKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.Tooltip")]
     public DisplayType DisplayTypes;
-
-    public enum DisplayType
-    {
-        [LabelKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.Deaths.Label")]
-        [TooltipKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.Deaths.Tooltip")]
-        Deaths,
-
-        [LabelKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.DeathsCauses.Label")]
-        [TooltipKey("$Mods.DeathLeaderboard.Configs.Common.DisplayTypes.DeathsCauses.Tooltip")]
-        DeathsCauses
-    }
 }
